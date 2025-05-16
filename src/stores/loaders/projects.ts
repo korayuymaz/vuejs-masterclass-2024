cccd89f73eaecc875ee377b46f3c60a8a2ef27e2
Koray <koray.uymaz@agesa.com.tr>
2025-05-16 22:49:13 +0300

    Change data after update

--- src/stores/loaders/projects.ts
+++ src/stores/loaders/projects.ts
@@ -3,17 +3,18 @@ import { useMemoize } from '@vueuse/core'
 import type { Projects } from '@/utils/supaQueries'
 
 export const useProjectsStore = defineStore('projects-store', () => {
-  const projects = ref<Projects | null>(null)
+  const projects = ref<Projects>([])
 
   const loadProjects = useMemoize(async (key: string) => await projectsQuery)
 
   const validateCache = () => {
     if (projects.value?.length) {
-      projectsQuery.then(({ data }) => {
+      projectsQuery.then(({ data, error }) => {
         if (JSON.stringify(projects.value) === JSON.stringify(data)) {
           return
         } else {
           loadProjects.delete('projects')
+          if (!error && data) projects.value = data
         }
       })
     }
@@ -23,7 +24,7 @@ export const useProjectsStore = defineStore('projects-store', () => {
     const { data, error, status } = await loadProjects('projects')
     if (error) useErrorStore().setError({ error, customCode: status })
 
-    projects.value = data
+    if (data) projects.value = data
 
     validateCache()
   }
