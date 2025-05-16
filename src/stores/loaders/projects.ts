6c91dde3d35d4c1dc1006f630b936db8c2901f33
Koray <koray.uymaz@agesa.com.tr>
2025-05-17 00:25:03 +0300

    Make the Pinia Loader Cache Invalidation Logic Reusable

--- src/stores/loaders/projects.ts
+++ src/stores/loaders/projects.ts
@@ -1,20 +1,38 @@
-import { projectsQuery } from '@/utils/supaQueries'
+import { projectQuery, projectsQuery } from '@/utils/supaQueries'
 import { useMemoize } from '@vueuse/core'
-import type { Projects } from '@/utils/supaQueries'
+import type { Project, Projects } from '@/utils/supaQueries'
 
 export const useProjectsStore = defineStore('projects-store', () => {
-  const projects = ref<Projects>([])
-
+  const projects = ref<Projects | null>(null)
+  const project = ref<Project | null>(null)
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const loadProjects = useMemoize(async (key: string) => await projectsQuery)
+  const loadProject = useMemoize(
+    async (slug: string) => await projectQuery(slug)
+  )
 
-  const validateCache = () => {
-    if (projects.value?.length) {
-      projectsQuery.then(({ data, error }) => {
-        if (JSON.stringify(projects.value) === JSON.stringify(data)) {
+  interface ValidateCacheParams {
+    ref: typeof projects | typeof project
+    query: typeof projectsQuery | typeof projectQuery
+    key: string
+    loaderFn: typeof loadProjects | typeof loadProject
+  }
+
+  const validateCache = ({
+    ref,
+    query,
+    key,
+    loaderFn
+  }: ValidateCacheParams) => {
+    if (ref.value) {
+      const finalQuery = typeof query === 'function' ? query(key) : query
+
+      finalQuery.then(({ data, error }) => {
+        if (JSON.stringify(ref.value) === JSON.stringify(data)) {
           return
         } else {
-          loadProjects.delete('projects')
-          if (!error && data) projects.value = data
+          loaderFn.delete(key)
+          if (!error && data) ref.value = data
         }
       })
     }
@@ -22,15 +40,38 @@ export const useProjectsStore = defineStore('projects-store', () => {
 
   const getProjects = async () => {
     const { data, error, status } = await loadProjects('projects')
+
     if (error) useErrorStore().setError({ error, customCode: status })
 
     if (data) projects.value = data
 
-    validateCache()
+    validateCache({
+      ref: projects,
+      query: projectsQuery,
+      key: 'projects',
+      loaderFn: loadProjects
+    })
+  }
+
+  const getProject = async (slug: string) => {
+    const { data, error, status } = await loadProject(slug)
+
+    if (error) useErrorStore().setError({ error, customCode: status })
+
+    if (data) project.value = data
+
+    validateCache({
+      ref: project,
+      query: projectQuery,
+      key: slug,
+      loaderFn: loadProject
+    })
   }
 
   return {
     projects,
-    getProjects
+    getProjects,
+    getProject,
+    project
   }
 })
--- src/utils/tableColumns/projectsColumns.ts
+++ src/utils/tableColumns/projectsColumns.ts
@@ -7,7 +7,9 @@ import Avatar from '@/components/ui/avatar/Avatar.vue'
 import AvatarImage from '@/components/ui/avatar/AvatarImage.vue'
 import AvatarFallback from '@/components/ui/avatar/AvatarFallback.vue'
 
-export const columns = (collabs: Ref<GroupedCollabs>):ColumnDef<Projects[0]>[] => [
+export const columns = (
+  collabs: Ref<GroupedCollabs>
+): ColumnDef<Projects[0]>[] => [
   {
     accessorKey: 'name',
     header: () => h('div', { class: 'text-left' }, 'Name'),
@@ -26,7 +28,11 @@ export const columns = (collabs: Ref<GroupedCollabs>):ColumnDef<Projects[0]>[] =
     accessorKey: 'status',
     header: () => h('div', { class: 'text-left' }, 'Status'),
     cell: ({ row }) => {
-      return h('div', { class: 'text-left font-medium' }, row.getValue('status'))
+      return h(
+        'div',
+        { class: 'text-left font-medium' },
+        row.getValue('status')
+      )
     }
   },
   {
@@ -35,16 +41,22 @@ export const columns = (collabs: Ref<GroupedCollabs>):ColumnDef<Projects[0]>[] =
     cell: ({ row }) => {
       return h(
         'div',
-        { class: 'text-left font-medium' },
-        collabs.value[row.original.id] ?
-        collabs.value[row.original.id].map((collab) => {
-          return h(RouterLink, {to: `/users/${collab.username}`}, () => {
-            return h(Avatar, {class: "hover:scale-110 transition-transform"}, () =>
-              h(AvatarImage, {src: collab.avatar_url || ''}))
-          })
-        }) : row.original.collaborators.map(() => {
-          return h(Avatar, {class: "animate-pulse"}, () => h(AvatarFallback))
-        })
+        { class: 'text-left font-medium h-20 flex items-center' },
+        collabs.value[row.original.id]
+          ? collabs.value[row.original.id].map((collab) => {
+              return h(RouterLink, { to: `/users/${collab.username}` }, () => {
+                return h(
+                  Avatar,
+                  { class: 'hover:scale-110 transition-transform' },
+                  () => h(AvatarImage, { src: collab.avatar_url || '' })
+                )
+              })
+            })
+          : row.original.collaborators.map(() => {
+              return h(Avatar, { class: 'animate-pulse' }, () =>
+                h(AvatarFallback)
+              )
+            })
       )
     }
   }
