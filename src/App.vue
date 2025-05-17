bdec8b7c687656c418679bff27faa4ca1892cd72
Koray <koray.uymaz@agesa.com.tr>
2025-05-17 17:46:09 +0300

    Guest Layout

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,37 @@
 <script setup lang="ts">
-
 const errorStore = useErrorStore()
 
-onErrorCaptured(error => {
+onErrorCaptured((error) => {
   errorStore.setError({ error })
 })
 
 onMounted(() => {
   useAuthStore().trackAuthChanges()
 })
+
+const { user } = storeToRefs(useAuthStore())
+
+const AuthLayout = defineAsyncComponent(
+  () => import('./components/Layout/main/AuthLayout.vue')
+)
+
+const GuestLayout = defineAsyncComponent(
+  () => import('./components/Layout/main/GuestLayout.vue')
+)
 </script>
 
 <template>
-  <AuthLayout>
+  <Component :is="user ? AuthLayout : GuestLayout">
     <AppErrorPage v-if="errorStore.activeError" />
-    <RouterView v-slot="{ Component, route }">
+
+    <RouterView v-else v-slot="{ Component, route }">
       <Suspense v-if="Component" :timeout="0">
-        <Component :is="Component" :key="route.name"> </Component>
+        <Component :is="Component" :key="route.name"></Component>
+
         <template #fallback>
-          <div>
-            <!-- <LoadingSpinner /> -->
-            Loading..
-          </div>
+          <span>Loading ..</span>
         </template>
       </Suspense>
     </RouterView>
-  </AuthLayout>
+  </Component>
 </template>
